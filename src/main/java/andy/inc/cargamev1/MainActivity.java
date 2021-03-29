8a382b057aa3dbabb4f06242eba0a07c02831ee7
Andy K <77582632+sathvikkurap@users.noreply.github.com>
2021-03-29 10:29:32 -0700

    Update MainActivity.java
    More advanced logic

--- src/main/java/andy/inc/cargamev1/MainActivity.java
+++ src/main/java/andy/inc/cargamev1/MainActivity.java
@@ -1,15 +1,13 @@
 package andy.inc.cargamev1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.DisplayMetrics;
 import android.view.Display;
-import android.view.WindowManager;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,11 +16,16 @@ public class MainActivity extends Activity {
         Handler handler = new Handler();
         CarView carView;
         Display display;
-        static Point size;
+    private static Context mContext;
+    static Point size;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mContext = this.getApplicationContext();
         display = getWindowManager().getDefaultDisplay();
 
         size = new Point();
@@ -33,15 +36,23 @@ public class MainActivity extends Activity {
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        carView.invalidate();
-                    }
-                });
-            }
-        }, 0, 30);
+                           @Override
+                           public void run() {
+                               handler.post(new Runnable() {
+                                   @Override
+                                   public void run() {
+                                       carView.invalidate();
+                                   }
+                               });
+                           }
+                       },
+                0,
+                30);
+    }
+    protected static void gameOverActivity(){
+        Toast.makeText(mContext, "Please wait...", Toast.LENGTH_SHORT).show();
+        Intent login = new Intent(mContext, GameOverActivity.class);
+        mContext.startActivity(login);
+
     }
 }
